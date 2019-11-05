ed506ce7a8a37dc1d31582d3506c54233f16ce46
ezesundayeze <mailsforeze@gmail.com>
2019-11-05 12:16:37 +0100

    made installable via pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'ngpayetax',
+  packages = ['ngpayetax'],
+  version = '0.1',
+  license='MIT',
+  description = 'This package allows you to calculate Pay as You Earn tax for your employees in Nigeria',   # Give a short description about your library
+  author = 'Eze Sunday Eze',
+  author_email = 'mailstoeze@gmail.com',
+  url = 'https://github.com/ezesundayeze/paye-tax',
+  download_url = "https://github.com/ezesundayeze/paye-tax/archive/master.zip",
+  keywords = ['paye tax in nigeria', 'paye tax', 'tax'],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
